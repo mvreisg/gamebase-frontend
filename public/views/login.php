7941e984bed6262ff6274049e6c78691ba3dc0f8
Marcus Vinicius <marcus_viniciusreis@hotmail.com>
2025-03-15 18:25:03 -0300

    [update] better spaced login interface

--- public/views/login.php
+++ public/views/login.php
@@ -10,34 +10,42 @@
     </head>
     <body>
         <div id="login-balloon-container">
-            <form id="login-balloon-box">                
+            <form id="login-balloon-box" method="POST">                
                 <h1>Gamebase</h1>                
                 <div id="login-balloon-texts">
-                    <div class="text-div">
-                        <label class="text-label" for="username">
-                            <input id="username" name="username" type="text" placeholder="Seu nome de usuário..." />
+                    <div class="text-input-div">
+                        <label class="text-input-label" for="username">
+                            <input id="username" name="username" type="text" placeholder="Seu nome de usuário..." required />
                         </label>
-                    </div>         
-                    <div class="text-div">
-                        <label class="text-label" for="password">
-                            <input id="password" name="password" type="password" placeholder="Sua senha..." />
+                    </div>     
+                    <div id="username-warning-div" class="warning-text">
+                        <p>O username não foi informado!</p>
+                    </div>    
+                    <div class="text-input-div">
+                        <label class="text-input-label" for="password">
+                            <input id="password" name="password" type="password" placeholder="Sua senha..." required />
                         </label>
                         <button type="button" onclick="togglePasswordVisibility();">
                             <img id="password-visibility-indicator" src="./assets/svg/eye-open-white.svg"/>
                         </button>                    
+                    </div>
+                    <div id="password-warning-div" class="warning-text">
+                        <p>A senha não foi informada!</p>
                     </div>    
-                </div>                       
-                <div id="one-week-checkbox-div">
-                    <label id="one-week-checkbox-label" class="unchecked-checkbox" for="password">
-                        <input id="one-week-checkbox" name="one-week-checkbox" type="checkbox" onclick="toggleOneWeekCheckboxState();" />
-                    </label>
-                    <span>Lembrar de mim por uma semana</span>
                 </div>
-                <label id="login-button-label" for="submit">
-                    <button id="login-button" name="submit" type="button">
-                        <span>Entrar</span>                   
-                    </button>
-                </label>            
+                <div id="login-confirmations-box">
+                    <div id="one-week-checkbox-div">
+                        <label id="one-week-checkbox-label" class="unchecked-checkbox" for="password">
+                            <input id="one-week-checkbox" name="one-week-checkbox" type="checkbox" onclick="toggleOneWeekCheckboxState();" />
+                        </label>
+                        <span>Lembrar de mim por uma semana</span>
+                    </div>
+                    <label id="login-button-label" for="submit">
+                        <button id="login-button" name="submit" type="submit">
+                            <span>Entrar</span>                   
+                        </button>
+                    </label>            
+                </div>                                     
             </form>
         </div>        
     </body>
