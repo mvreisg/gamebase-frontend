289e12a531e1ffeb45637251422f56ba10b857c8
Marcus Vinicius <marcus_viniciusreis@hotmail.com>
2025-03-31 15:15:15 -0300

    [update] added further instructions to after_install.sh script

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo mkdir /var/www/html/public/config
+sudo cp /home/ubuntu/environment.json /var/www/html/public/config/environment.json
 cd /var/www/html
 sudo docker compose down
 sudo docker compose up --build -d
